0288634aa0399d03d491c73bd6e478e720895581
Tay CJ <cjtay@outlook.sg>
2021-06-21 19:11:23 +0800

    display user info, added logout

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 
 function App() {
-    const { loginWithRedirect } = useAuth0();
+    const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+    if (user) {
+        console.log('user: ', user);
+    }
     return (
         <div className='App'>
             <header className='App-header'>
                 <h1>Authentication template using Auth0</h1>
-                <p onClick={loginWithRedirect}>Login</p>
+                {isAuthenticated ? (
+                    <button onClick={logout}>Logout</button>
+                ) : (
+                    <button onClick={loginWithRedirect}>Login</button>
+                )}
+
                 <a
                     className='App-link'
                     href='https://auth0.com/docs/quickstart/spa/react'
@@ -18,6 +26,15 @@ function App() {
                 >
                     Auth0 docs
                 </a>
+                {user && (
+                    <>
+                        <div>Name: {user.name}</div>
+                        <div>Email {user.email}</div>
+                        <div>Role: {user.roles}</div>
+                        <div>Nickname: {user.nickname}</div>
+                        <img src={user.picture} alt='user' />
+                    </>
+                )}
             </header>
         </div>
     );
