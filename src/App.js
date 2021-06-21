071292e88fff9aa3f48edbcd96d39f5facff8c8b
Tay CJ <cjtay@outlook.sg>
2021-06-21 17:40:07 +0800

    initial commit added auth0 till loginwithredirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loginWithRedirect } = useAuth0();
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <h1>Authentication template using Auth0</h1>
+                <p onClick={loginWithRedirect}>Login</p>
+                <a
+                    className='App-link'
+                    href='https://auth0.com/docs/quickstart/spa/react'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Auth0 docs
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 import './index.css';
 import App from './App';
+import Dashboard from './components/Dashboard';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Auth0Provider
+            domain='dev-kcs-n29r.au.auth0.com'
+            clientId='kq77Q978g7x21DZb27VND7jLZqw4J4e4'
+            redirectUri={window.location.origin}
+        >
+            <Router>
+                <Route exact path='/'>
+                    <App />
+                </Route>
+                <Route exact path='/dashboard'>
+                    <Dashboard />
+                </Route>
+            </Router>
+        </Auth0Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
