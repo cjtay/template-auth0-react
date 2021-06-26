c9ece27e8a6a99fcbff1578d17a6aa7366467a0b
Tay CJ <cjtay@outlook.sg>
2021-06-26 16:23:54 +0800

    adjusted namespace

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,36 @@
-import React, { useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import {FetchContext} from '../context/FetchContext'
+import { FetchContext } from '../context/FetchContext';
 
 const Dashboard = () => {
-    const fetchContext = useContext(FetchContext)
+    const fetchContext = useContext(FetchContext);
     const [basicData, setBasicData] = useState();
 
-    const { user, isAuthenticated, logout, isLoading } =
-        useAuth0();
-        
+    const { user, isAuthenticated, logout, isLoading } = useAuth0();
+
     let roles;
     if (user) {
-        roles = user[`http://localhost:3000/roles`];
+        roles = user[`http://digitalfunnel/roles`];
         console.log('user role: ', roles);
     }
 
     useEffect(() => {
         const getData = async () => {
-          try {
-            const { data } = await fetchContext.authAxios.get(
-              'http://localhost:8000/api/'
-            );
-            setBasicData(data);
-          } catch (err) {
-            console.log('API error: ',err);
-          }
+            try {
+                const { data } = await fetchContext.authAxios.get(
+                    'http://localhost:8000/api/user'
+                );
+                const formattedData = JSON.stringify(data);
+                setBasicData(formattedData);
+                console.log('basic data: ', basicData);
+            } catch (err) {
+                console.log('API error: ', err);
+            }
         };
-    
-        getData();
-      }, [fetchContext]);
 
+        getData();
+    }, [fetchContext, basicData]);
 
     return (
         <>
@@ -38,54 +38,54 @@ const Dashboard = () => {
                 <h2>Loading ......</h2>
             ) : (
                 <>
-                    <h2 className='px-5 py-2 text-center bg-gray-200'>
+                    <h2 className="px-5 py-2 text-center bg-gray-200">
                         Dashboard Component
                     </h2>
                     {!isAuthenticated && (
                         <>
-                            <h2 className='p-4 bg-red-500'>
+                            <h2 className="p-4 bg-red-500">
                                 You are not login, please return to main page
                             </h2>
-                            <Link to='/'>Go Home</Link>
+                            <Link to="/">Go Home</Link>
                         </>
                     )}
                     {isAuthenticated && user && (
-                        <div className='w-6/12 p-3 mx-auto my-5 bg-green-100'>
+                        <div className="w-6/12 p-3 mx-auto my-5 bg-green-100">
                             <div>
-                                <span className='font-bold'>Name: </span>
+                                <span className="font-bold">Name: </span>
                                 {user.name}
                             </div>
                             <div>
-                                <span className='font-bold'>Email: </span>
+                                <span className="font-bold">Email: </span>
                                 {user.email}
                             </div>
                             <div>
-                                <span className='font-bold'>Role: </span>
+                                <span className="font-bold">Role: </span>
                                 {roles[0]}
                             </div>
                             <div>
-                                <span className='font-bold'>Nickname: </span>
+                                <span className="font-bold">Nickname: </span>
                                 {user.nickname}
                             </div>
                             <div>
-                                <span className='font-bold'>API Data: </span>
+                                <span className="font-bold">API Data: </span>
                                 {basicData}
                             </div>
                             <img
-                                className='my-3'
+                                className="my-3"
                                 src={user.picture}
-                                alt='user'
+                                alt="user"
                             />
-                            <div className='overflow-scroll'>
-                                <span className='font-bold'>
+                            <div className="overflow-scroll">
+                                <span className="font-bold">
                                     Access Token:{' '}
                                 </span>
                                 {fetchContext.accessToken}
                             </div>
-                            
+
                             <div>
                                 <button
-                                    className='btn-dark'
+                                    className="btn-dark"
                                     onClick={() => {
                                         logout({
                                             returnTo:
